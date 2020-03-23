f0287a1b3a570eee068637153800529531180256
ivan.star <vandv1337@mail.ru>
2020-03-23 18:55:43 +0500

    made the code a little more readable and tweaked drug and drop

--- main.js
+++ main.js
@@ -1,39 +1,108 @@
-var k=0
-var boardm = [['BR','BK','BB','BQ','BKG','BB','BK','BR'],
-        ['BP','BP','BP','BP','BP','BP','BP','BP'],
-        ['0','0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0','0'],
-        ['0','0','0','0','0','0','0','0'],
-        ['WP','WP','WP','WP','WP','WP','WP','WP'],
-        ['WR','WK','WB','WQ','WKG',"WB",'WK','WR'] 
-    ];
-function refresh() {
+let index1;
+let isPPressed = 0; // для хранения состояния элемента вывода ходов на экран
+let progressСhecker = 0; // для хранения очередности ходов
+let recordingMovesCounter = -1; // для оформления в элементе вывода ходов а экран 
+let table = document.getElementById('table');
+let p = document.querySelector('p');
+let boardm = [['BR','BK','BB','BQ','BKG','BB','BK','BR'],
+              ['BP','BP','BP','BP','BP','BP','BP','BP'],
+              ['0','0','0','0','0','0','0','0'],
+              ['0','0','0','0','0','0','0','0'],
+              ['0','0','0','0','0','0','0','0'],
+              ['0','0','0','0','0','0','0','0'],
+              ['WP','WP','WP','WP','WP','WP','WP','WP'],
+              ['WR','WK','WB','WQ','WKG',"WB",'WK','WR'] 
+             ];
+             
+p.onmouseup = function(){                                                           // для изменения вида вывода ходов на экран
+    if (isPPressed === 0 ) {
+        p.classList.add('pressedP');
+        isPPressed = 1;
+    }
+    else {
+        p.classList.remove('pressedP');
+        isPPressed = 0;
+    }
+}
+table.onmousedown = function(event) {
+    let target = event.target;
+    if (progressСheck(target.className) === false) {return;}
+    if ( target.nodeName === "BODY" || target.nodeName === "DIV" || target.nodeName === "TABLE" || target.nodeName === "P" || target.nodeName === "HTML") { refresh();return; }
+    if ( target.nodeName === "TH" ) { return; }
+    if (event.which != 1) return;                                                  //отменяет выполнение функции при выделении не тех фигур или не фигур
+    
+    target.hidden = true;
+    var index1 = document.elementFromPoint(event.clientX, event.clientY);          //для захвата захвата id элемента под фигурой 
+    target.hidden = false;
+    
+    target.classList.add('highlight');                                             // для увелечения фигуры при взоимодействии 
+    target.style.position = 'absolute';
+    moveAt(event);                                                                 
+    table.appendChild(target);
+    target.style.zIndex = 1000;
+    function moveAt(event) {                                                       // для отвлеживания координат фигуры при перемещении 
+        target.style.left = event.pageX - target.offsetWidth/2+ 'px';              
+        target.style.top = event.pageY - target.offsetHeight/2+ 'px';
+    }
+    target.ondragstart = function() {                                              //для коректного захвата элемента мышкой 
+        return false;
+      }
+    document.onmousemove = function(event) {
+        moveAt(event);
+    }
+    table.onmouseup = function(e) {                                                // для отследивания конца перемещения и помещения фигуры в нужное место
+        
+        refresh();
+        target.hidden = true;
+        let index2 = document.elementFromPoint(e.clientX, e.clientY);
+        if ( index2.nodeName === "BODY" || index2.nodeName === "DIV" || index2.nodeName === "TABLE" || index2.nodeName === "P" || index2.nodeName === "HTML") { refresh();return; }
+        index2 = ebaniyDADrop(index2, e);
+        choise(index1.id, index2.id);
+        document.onmousemove = null;
+        target.onmousup = null;
+        target  = {};
+        
+        return;
+        
+
+
+    }
+    function ebaniyDADrop(index, e) {                                             // для нахождения id элемента на который предпологается перемещение фигуры 
+        if ( index.nodeName !== "TH" ) {
+            index.hidden = true;
+            index = document.elementFromPoint(e.clientX, e.clientY);
+            ebaniyDADrop(index, e);
+        }
+        return index;
+        }
+
+}  
+function refresh() {                                                              // для обновления таблицы на экране вывода в начале работы программы и при перемещении фигур
     for ( var i = 0; i<8; i++ ) {
         for ( var j = 0; j<8; j++ ) {
             switch(boardm[j][i]){
-                case '0' : document.getElementById(''+i+j).innerHTML = "&nbsp"; break;
-                case 'BP' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bP.png" class = "draggable">'; break;
-                case 'WP' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wP.png" class = "draggable">'; break;
-                case 'WKG': document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wK.png" class = "draggable">'; break;
-                case 'BKG': document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bK.png" class = "draggable">'; break;
-                case 'BK' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bN.png" class = "draggable">'; break;
-                case 'WK' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wN.png" class = "draggable">'; break;
-                case 'WR' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wR.png" class = "draggable">'; break;
-                case 'BR' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bR.png" class = "draggable">'; break;
-                case 'BB' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bB.png" class = "draggable">'; break;
-                case 'WB' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wB.png" class = "draggable">'; break;
-                case 'WQ' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wQ.png" class = "draggable">'; break;
-                case 'BQ' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bQ.png" class = "draggable">'; break;
+                case '0'  : document.getElementById(''+i+j).innerHTML = "&nbsp"; break;
+                case 'BP' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bP.png" class = "black">'; break;
+                case 'WP' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wP.png" class = "white">'; break;
+                case 'WKG': document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wK.png" class = "white">'; break;
+                case 'BKG': document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bK.png" class = "black">'; break;
+                case 'BK' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bN.png" class = "black">'; break;
+                case 'WK' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wN.png" class = "white">'; break;
+                case 'WR' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wR.png" class = "white">'; break;
+                case 'BR' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bR.png" class = "black">'; break;
+                case 'BB' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bB.png" class = "black">'; break;
+                case 'WB' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wB.png" class = "white">'; break;
+                case 'WQ' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/wQ.png" class = "white">'; break;
+                case 'BQ' : document.getElementById(''+i+j).innerHTML = '<img src = "./iconchess/bQ.png" class = "black">'; break;
             }
         
-        document.getElementById(''+i+j).classList.remove('highlight');
+        document.getElementById(''+i+j).classList.remove('highlight');              // возвращения фигуры к нормальному размеру после перемещения 
         
         }
     }
     
 }
-function arrayEntry(selectedCell,wtMove,Figure) {
+function arrayEntry(selectedCell,wtMove,Figure) {                                   // для записи в массив boardm корректного местоположения фигур
     progressСhecker = Figure.get(3);
     recordingMoves(selectedCell,wtMove);
     win(wtMove);
@@ -42,7 +111,7 @@ function arrayEntry(selectedCell,wtMove,Figure) {
     refresh();
     
 }
-function win(wtMove,itIsKing) {
+function win(wtMove,itIsKing) {                                                     // для проверки не был ли убит король этим ходом 
     
     if (boardm[wtMove[1]][wtMove[0]] == 'WKG' && progressСhecker == 0) {
         alert( "Black Win, gg ez");
@@ -55,19 +124,14 @@ function win(wtMove,itIsKing) {
         document.getElementById('body').classList.add('win');
     }
 }
-let table = document.getElementById('table');
-let p = document.querySelector('p');
-refresh();
-let progressСhecker = 0;
-let recordingMovesCounter = -1;
-function recordingMoves(selectedCell,wtMove){
+function recordingMoves(selectedCell,wtMove){                                       // для оформления вывода в элементе вывода ходов на экран 
     let textInPre = p.innerHTML;
     let FiguraFoRecording;
     switch (boardm[+(selectedCell[1])][+(selectedCell[0])]) {
-        case 'BP' : FiguraFoRecording  = "&#9823;"; break;
+        case 'BP': FiguraFoRecording  = "&#9823;"; break;
         case 'WP' : FiguraFoRecording = "&#9817;"; break;
-        case 'WKG' : FiguraFoRecording = "&#9812;"; break;
-        case 'BKG' : FiguraFoRecording = "&#9818;"; break;
+        case 'WKG': FiguraFoRecording = "&#9812;"; break;
+        case 'BKG': FiguraFoRecording = "&#9818;"; break;
         case 'BK' : FiguraFoRecording = "&#9822;"; break;
         case 'WK' : FiguraFoRecording = "&#9816;"; break;
         case 'WR' : FiguraFoRecording = "&#9814;"; break;
@@ -75,7 +139,7 @@ function recordingMoves(selectedCell,wtMove){
         case 'BB' : FiguraFoRecording = "&#9821;"; break;
         case 'WB' : FiguraFoRecording = "&#9815;"; break;
         case 'WQ' : FiguraFoRecording = "&#9813;"; break;
-        case 'BQ': FiguraFoRecording = "&#9819;"; break;
+        case 'BQ' : FiguraFoRecording = "&#9819;"; break;
     }
     if( recordingMovesCounter === 4) {
         textInPre =  FiguraFoRecording + "|" + selectedCell[1] + selectedCell[0] + ' --> ' + wtMove[1] + wtMove[0] + "|" + "&nbsp;&nbsp;&nbsp;&nbsp;" + "<br >"+ textInPre;
@@ -87,110 +151,13 @@ function recordingMoves(selectedCell,wtMove){
     }
     p.innerHTML = textInPre;
 }
-var index1;
-
-let isPPressed = 0;
-p.onmouseup = function(){
-    if (isPPressed === 0 ) {
-        p.classList.add('pressedP');
-        isPPressed = 1;
-    }
-    else {
-        p.classList.remove('pressedP');
-        isPPressed = 0;
-    }
-}
-table.onmousedown = function(event) {
-    let target = event.target;
-    
-    if ( target.nodeName === "TH" ) { return; }
-    if ( target.innerHTML === "&nbsp;") {return; }
-    if (event.which != 1) return;
-    target.hidden = true;
-    // if (progressСheck(boardm[ +( target.id[1] ) ] [ +( target.id[0] ) ][0]) === false) return;
-    var index1 = document.elementFromPoint(event.clientX, event.clientY);
-    target.hidden = false;
-    if (progressСheck( boardm[index1.id[1]][index1.id[0]][0]) === false) {
-    refresh();return;
-    }
-    target.style.position = 'absolute';
-    moveAt(event);
-    table.appendChild(target);
-    target.style.zIndex = 1000;
-    function moveAt(event) {
-        target.style.left = event.pageX - target.offsetWidth/2+ 'px';
-        target.style.top = event.pageY - target.offsetHeight/2+ 'px';
-    }
-    target.ondragstart = function() {
-        return false;
-      }
-    document.onmousemove = function(event) {
-        moveAt(event);
-    }
-    table.onmouseup = function(e) {
-        
-        
-        target.hidden = true;
-        let index2 = document.elementFromPoint(e.clientX, e.clientY);
-        if ( index2.nodeName === "BODY" || index2.nodeName === "DIV" || index2.nodeName === "TABLE" || index2.nodeName === "P" || index2.nodeName === "HTML") { refresh();return; }
-        index2 = ebaniyDADrop(index2, e);
-        choise(index1.id, index2);
-        document.onmousemove = null;
-        target.onmousup = null;
-        target  = {};
-        
-        return;
-        
-
-
-    }
-    function ebaniyDADrop(index, e) { 
-    if ( index.nodeName !== "TH" ) {
-        index.hidden = true;
-        index = document.elementFromPoint(e.clientX, e.clientY);
-        ebaniyDADrop(index, e);
-    }
-    return index.id;
-    }
-
-}
-
-
-
-// table.onmouseup = function(event) {
-
-//     let target = event.target;
-//     if ( k === 2 ) {
-        
-//         target.classList.remove('highlight');
-//         k = 0;
-//     }
-//     if ( k === 1 ) {
-        
-//         target.classList.remove('highlight'); 
-//         index2 = target.id;
-//         k++;
-//         var elementToChange = document.getElementsByTagName("body")[0];
-//         elementToChange.style.cursor = "default";
-//         choise(index1, index2); 
-//     }
-//     if ( k==0 && boardm[ +( target.id[1] ) ] [ +( target.id[0] ) ] != '0') {
-//         if (progressСheck(boardm[ +( target.id[1] ) ] [ +( target.id[0] ) ][0])) {
-//             target.classList.add('highlight');
-//             index1 = target.id;
-//             k++;
-            
-//         }
-//     }      
-// }               
-
-function progressСheck(element) {
+function progressСheck(element) {                                                    // проверяет какая сторона должна ходить в данный момент
     if (progressСhecker == 2) return false;
-    else if ( progressСhecker%2 == 0 && element == "W") return true;
-    else if ( progressСhecker%2 == 1 && element == "B") return true;
+    else if ( progressСhecker%2 == 0 && element == "white") return true;
+    else if ( progressСhecker%2 == 1 && element == "black") return true;
     else return false;
 }
-function choise(selectedCell, wtMove) {
+function choise(selectedCell, wtMove) {                                              // проверяет какого типа фигура ходит в данный момент 
     switch ( boardm[+(selectedCell[1])] [+(selectedCell[0])] ){
         case "BP" : BPmoveANDcheck(selectedCell, wtMove); break;
         case "WP" : WPmoveANDcheck(selectedCell, wtMove); break;
@@ -414,3 +381,4 @@ function BishopMoveANDcheck(selectedCell,wtMove, Figure) {
     }
     refresh();
 }
+refresh();
